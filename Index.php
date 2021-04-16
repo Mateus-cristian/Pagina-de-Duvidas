175bcd917b2b5e2251a1434be1ad78feeb1d8060
Mateus Cristian <mateus_cristian@outlook.com>
2021-04-16 17:41:18 -0300

    A
    Refeito o index com php

---
+++ Index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./css/Style.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <header>
+        <div class="container header-bg">
+            <div class="header">
+                <span class="mobile-btn"></span>
+                <div class="menu-bg">
+                    <div class="menu">
+                        <div class="editar">
+                            <img src="./img/icones/editar.png">
+                            <a href="#">Seja um franqueado</a>
+                        </div>
+                        <div class="consultor">
+                            <img src="./img/icones/seja_consultor.png">
+                            <a href="#">Seja um consultor(a)</a>
+                        </div>
+                        <div class="comprar">
+                            <a href="#"><b>Quero Comprar</b></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="area-restrita">
+                    <img src="./img/icones/area_restrita.png">
+                    <a href="#">Area Restrita</a>
+                </div>
+                <h1>Dúvidas</h1>
+                <p><b>Listamos aqui algumas duvidas frequentes</b></p>
+                <p>caso a sua dúvida não esteja aqui você pode<br> entrar em contato conosco <a href="#">clicando aqui</a></p>
+            </div>
+        </div>
+    </header>
+
+    <?php include 'tarefas.php'; ?>
+
+    <footer></footer>
+
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
